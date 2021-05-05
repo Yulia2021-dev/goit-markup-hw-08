c16b2f0b10bf4236d7744f8d947ae2c122646738
Yulia Kulyas <ulaboyko@ukr.net>
2021-05-05 23:07:49 +0300

    Fixin mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,26 @@
-;(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]")
-  const mobileMenuRef = document.querySelector("[data-menu]")
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]")
+//   const mobileMenuRef = document.querySelector("[data-menu]")
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
 
-    menuBtnRef.classList.toggle("is-open")
-    menuBtnRef.setAttribute("aria-expanded", !expanded)
+//     menuBtnRef.classList.toggle("is-open")
+//     menuBtnRef.setAttribute("aria-expanded", !expanded)
 
-    mobileMenuRef.classList.toggle("is-open")
-  })
-})()
+//     mobileMenuRef.classList.toggle("is-open")
+//   })
+// })();
+
+const hamburger = document.querySelector(".hamburger");
+const mobileMenuRef = document.querySelector(".mobile-menu");
+const logo = document.querySelector(".logo");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("toggle");
+
+    mobileMenuRef.classList.toggle("toggle");
+    logo.classList.toggle("hidden");
+}
