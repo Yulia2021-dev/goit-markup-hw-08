7310440944e4b339b340965efd767c5cdfa171d4
Yulia Kulyas <ulaboyko@ukr.net>
2021-05-09 21:56:25 +0300

    Complete adaptive styles

--- js/menu.js
+++ js/menu.js
@@ -1,26 +1,30 @@
-// (() => {
-//   const menuBtnRef = document.querySelector("[data-menu-button]")
-//   const mobileMenuRef = document.querySelector("[data-menu]")
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]")
+  const mobileMenuRef = document.querySelector("[data-menu]")
 
-//   menuBtnRef.addEventListener("click", () => {
-//     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+  menuBtnRef.addEventListener("click", () => {
+    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
 
-//     menuBtnRef.classList.toggle("is-open")
-//     menuBtnRef.setAttribute("aria-expanded", !expanded)
+    menuBtnRef.classList.toggle("is-open")
+    menuBtnRef.setAttribute("aria-expanded", !expanded)
 
-//     mobileMenuRef.classList.toggle("is-open")
-//   })
-// })();
+    mobileMenuRef.classList.toggle("is-open")
+  })
+})();
 
-const hamburger = document.querySelector(".hamburger");
-const mobileMenuRef = document.querySelector(".mobile-menu");
-const logo = document.querySelector(".logo");
+const hamburgerOpen = document.getElementById("btn-open");
+const hamburgerClose = document.getElementById("btn-close");
+const mobileMenuRef = document.querySelector("[data-menu]");
 
-hamburger.addEventListener("click", mobileMenu);
+hamburgerOpen.onclick = mobileMenu;
+
+hamburgerClose.onclick = mobileMenu;
+
 
 function mobileMenu() {
-    hamburger.classList.toggle("toggle");
+    //hamburger.classList.toggle("toggle");
 
+   // mobileMenuRef.classList.remove("toggle");
     mobileMenuRef.classList.toggle("toggle");
-    logo.classList.toggle("hidden");
+    // logo.classList.toggle("hidden");
 }
