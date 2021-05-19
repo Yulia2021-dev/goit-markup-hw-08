e0d832e6860730236c288dda78773b6cd7e98325
Yulia Kulyas <ulaboyko@ukr.net>
2021-05-19 23:48:11 +0300

    Fixed bugs on adaptive in 8 home task

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,21 @@
-(() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]")
-  const mobileMenuRef = document.querySelector("[data-menu]")
+// (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]")
+//   const mobileMenuRef = document.querySelector("[data-menu]")
 
-  menuBtnRef.addEventListener("click", () => {
-    const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
 
-    menuBtnRef.classList.toggle("is-open")
-    menuBtnRef.setAttribute("aria-expanded", !expanded)
+//     menuBtnRef.classList.toggle("is-open")
+//     menuBtnRef.setAttribute("aria-expanded", !expanded)
 
-    mobileMenuRef.classList.toggle("is-open")
-  })
-})();
+//     mobileMenuRef.classList.toggle("is-open")
+//   })
+// })();
 
 const hamburgerOpen = document.getElementById("btn-open");
 const hamburgerClose = document.getElementById("btn-close");
 const mobileMenuRef = document.querySelector("[data-menu]");
+// mobileMenuRef.classList.remove("toggle");
 
 hamburgerOpen.onclick = mobileMenu;
 
